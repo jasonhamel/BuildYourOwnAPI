a2f3cc9a2f59593813389bcfe22229b343951e9b
Jason <github@jasongetsemail.com>
2023-08-25 14:43:47 -0400

    Setup the get routes for /random /filter and /:id

--- index.js
+++ index.js
@@ -8,11 +8,32 @@ const masterKey = "4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT";
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //1. GET a random joke
-
+app.get("/random", (req, res) => {
+  const joke = Math.floor(Math.random() * jokes.length);
+  res.json(jokes[joke]);
+});
 //2. GET a specific joke
+app.get("/jokes/:id", (req, res) => {
+  const jokeID = req.params.id.slice(1, req.params.id.length);
+  if (jokeID > jokes.length - 1) {
+    res.json({
+      id:
+        "The ID" +
+        jokeID +
+        " is an invalid ID. Please try a number between 0 and 99",
+    });
+  }
 
+  res.json(jokes[jokeID]);
+});
 //3. GET a jokes by filtering on the joke type
-
+app.get("/filter", (req, res) => {
+  const type = req.query.type;
+  const jokeList = jokes.filter(
+    (joke) => joke.jokeType.toUpperCase() === type.toUpperCase()
+  );
+  res.json(jokeList);
+});
 //4. POST a new joke
 
 //5. PUT a joke
